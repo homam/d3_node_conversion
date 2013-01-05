085425d37e83624e9ed32d5c6ce37bff6e1950ff
Homam <bluesnowball@gmail.com>
2013-01-06 03:09:54 +0400

    treemaker

--- public/devices/TreeMaker.js
+++ public/devices/TreeMaker.js
@@ -18,8 +18,34 @@ var DeviceNode = (function () {
         if('string' == typeof args[0] && 'string' == typeof args[1]) {
             this.fallback = args[0];
             this.id = args[1];
+            this.visits = 0;
         }
+        this.children = [];
     }
+    DeviceNode.prototype.visitsIncludingChildren = function () {
+        if(isNaN(this._visitsIncludingChildren)) {
+            this._visitsIncludingChildren = this.visits + (this.children.length > 0 ? _(this.children.map(function (c) {
+                return c.visitsIncludingChildren();
+            })).reduce(function (a, b) {
+                return a + b;
+            }, 0) : 0);
+        }
+        return this._visitsIncludingChildren;
+    };
+    DeviceNode.prototype.subscribersIncludingChildren = function () {
+        if(isNaN(this._subscribersIncludingChildren)) {
+            this._subscribersIncludingChildren = (!!this.subMethods ? _(this.subMethods.map(function (s) {
+                return s.Subscribers;
+            })).reduce(function (a, b) {
+                return a + b;
+            }, 0) : 0) + (this.children.length > 0 ? _(this.children.map(function (c) {
+                return c.subscribersIncludingChildren();
+            })).reduce(function (a, b) {
+                return a + b;
+            }, 0) : 0);
+        }
+        return this._subscribersIncludingChildren;
+    };
     return DeviceNode;
 })();
 var TreeMaker = (function () {
@@ -34,14 +60,53 @@ var TreeMaker = (function () {
                 return n.id != fallbackid;
             });
         });
-        var parentedIds = _.uniq(nodes.map(function (n) {
-            return n.fallback;
-        })).filter(function (fallbackid) {
-            return nodes.some(function (n) {
-                return n.id == fallbackid;
+        nodes = nodes.concat(parentLessIds.map(function (id) {
+            return new DeviceNode(id == 'root' ? '' : 'root', id);
+        }));
+        if(nodes.every(function (n) {
+            return 'root' != n.id;
+        })) {
+            nodes.push(new DeviceNode('', 'root'))// root of the tree has no parent, add the root only if it has not been added yet
+            ;
+        }
+        // make the tree
+        nodes.forEach(function (node1) {
+            nodes.forEach(function (node2) {
+                if(node1.id == node2.fallback) {
+                    if(node1.fallback == node2.fallback) {
+                        throw JSON.stringify(node1) + "\n\n" + JSON.stringify(node2);
+                    }
+                    node1.children.push(node2);
+                }
             });
         });
-        console.log(nodes.length, parentLessIds.length, parentedIds.length);
+        // sort the tree
+        nodes = _(nodes).sortBy(function (n) {
+            return -n.visitsIncludingChildren();
+        });
+        var root = nodes[0];
+        console.log(root, TreeMaker.sortChildren(root));
+        $(document.body).append($("<ul></ul>").append(TreeMaker.renderTree(root)));
+    }
+    TreeMaker.sortChildren = function sortChildren(root) {
+        root.children = _(root.children).sortBy(function (n) {
+            return -n.visitsIncludingChildren();
+        });
+        root.children.forEach(function (c) {
+            return TreeMaker.sortChildren(c);
+        });
+        return root;
+    }
+    TreeMaker.renderTree = function renderTree(root) {
+        var li = $("<li></li>").append($("<span class='title'>").text(root.id)).append($("<span class='visits'>").text(root.visitsIncludingChildren())).append($("<span class='subscribers'>").text(root.subscribersIncludingChildren())).append($("<span class='conversion'>").text((Math.round(root.subscribersIncludingChildren() / root.visitsIncludingChildren() * 1000) / 10) + "%"));
+        if(root.children.length > 0) {
+            var ul = $("<ul></ul>");
+            root.children.forEach(function (c) {
+                ul.append(TreeMaker.renderTree(c));
+            });
+            li = li.append(ul);
+        }
+        return li;
     }
     return TreeMaker;
 })();
--- public/devices/TreeMaker.ts
+++ public/devices/TreeMaker.ts
@@ -13,6 +13,24 @@ class DeviceNode {
 
     public visits: number;
 
+    private _visitsIncludingChildren: number;
+    public visitsIncludingChildren():number {
+        if (isNaN(this._visitsIncludingChildren)) {
+            this._visitsIncludingChildren = this.visits +
+                (this.children.length > 0 ? _(this.children.map(c => c.visitsIncludingChildren())).reduce((a, b) => a + b, 0) : 0);
+        }
+        return this._visitsIncludingChildren;
+    }
+
+    private _subscribersIncludingChildren: number;
+    public subscribersIncludingChildren():number {
+        if (isNaN(this._subscribersIncludingChildren)) {
+            this._subscribersIncludingChildren = (!!this.subMethods ? _( this.subMethods.map(s=>s.Subscribers)).reduce((a,b)=>a+b,0) : 0) +
+                (this.children.length > 0 ? _(this.children.map(c => c.subscribersIncludingChildren())).reduce((a, b) => a + b, 0) : 0);
+        }
+        return this._subscribersIncludingChildren;
+    }
+
     constructor(record: IRecord);
     constructor(fallback: string, id: string);
     constructor();
@@ -25,7 +43,7 @@ class DeviceNode {
             this.fallback = record.Device.WURFL_FallBack;
             this.id = record.Device.WURFL_ID;
             this.os = record.Device.OS;
-            this.visits = record.Visits ?? 0;
+            this.visits = record.Visits;
 
             this.subMethods = record.Device.SubMethods;
 
@@ -36,7 +54,10 @@ class DeviceNode {
         if ('string' == typeof args[0] && 'string' == typeof args[1]) {
             this.fallback = args[0];
             this.id = args[1];
+            this.visits = 0;
         }
+
+        this.children = [];
     }
 }
 
@@ -45,9 +66,53 @@ class TreeMaker{
     constructor(records:IRecord[]){
         var nodes:DeviceNode[] = records.map(r => new DeviceNode(r));
         var parentLessIds = _.uniq(nodes.map(n => n.fallback)).filter(fallbackid => nodes.every(n => n.id != fallbackid));
-        var parentedIds = _.uniq(nodes.map(n => n.fallback)).filter(fallbackid => nodes.some(n => n.id == fallbackid));
+        nodes = nodes.concat(parentLessIds.map(id => new DeviceNode(id == 'root' ? '' :'root', id)));
+        if (nodes.every(n => 'root' != n.id)) {
+            nodes.push(new DeviceNode('', 'root')); // root of the tree has no parent, add the root only if it has not been added yet
+        }
 
-        console.log(nodes.length, parentLessIds.length, parentedIds.length);
+        // make the tree
+        nodes.forEach(node1 => {
+            nodes.forEach(node2 => {
+                if (node1.id == node2.fallback) {
+                    if(node1.fallback == node2.fallback)
+                        throw JSON.stringify(node1) + "\n\n" + JSON.stringify(node2);
+                    node1.children.push(node2);
+                }
+            });
+        });
+
+        // sort the tree
+        nodes =  _(nodes).sortBy((n: DeviceNode) => -n.visitsIncludingChildren());
+        var root = nodes[0];
+
+        console.log(root, TreeMaker.sortChildren(root));
+
+        $(document.body).append($("<ul></ul>").append( TreeMaker.renderTree(root)));
+
+    }
+
+    static sortChildren(root: DeviceNode): DeviceNode {
+        root.children = _(root.children).sortBy((n: DeviceNode) => -n.visitsIncludingChildren());
+        root.children.forEach(c => sortChildren(c));
+        return root;
+    }
+
+    static renderTree(root: DeviceNode):any {
+        var li = $("<li></li>")
+            .append($("<span class='title'>").text( root.id))
+            .append($("<span class='visits'>").text( root.visitsIncludingChildren()))
+            .append($("<span class='subscribers'>").text( root.subscribersIncludingChildren()))
+            .append($("<span class='conversion'>").text( (Math.round( root.subscribersIncludingChildren() / root.visitsIncludingChildren()*1000)/10) + "%"));
+
+        if (root.children.length > 0) {
+            var ul = $("<ul></ul>");
+            root.children.forEach(c => {
+                ul.append(TreeMaker.renderTree(c));
+            });
+            li = li.append(ul);
+        }
+        return li;
     }
 
 }
